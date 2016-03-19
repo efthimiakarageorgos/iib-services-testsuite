7dbe4d865a313fdf915d80a9bacd509a3795620c
Effie Karageorgos <effie.karageorgos@qiotec.com>
2016-03-19 16:15:03 -0400

    small changes

--- api/src/test/java/com/qio/assetManagement/manageAssettypes/ManageAssetTypes.java
+++ api/src/test/java/com/qio/assetManagement/manageAssettypes/ManageAssetTypes.java
@@ -38,28 +38,42 @@ public class ManageAssetTypes {
 	// RREHM-435 (AssetType abbreviation contains spaces)
 		@Test
 		public void shouldNotCreateAssetTypeWhenAbbrContainsSpaces() throws JsonGenerationException, JsonMappingException, IOException{
+			//Call a class that
+			//a. Make a call to form the appropriate for the test case asset type message
+			//b. Submit the request 
+			//c. returns the response of the request + request body
+			
+			
 			// Creating Asset-types
 			java.util.Date date= new java.util.Date();
 			String timestamp = Long.toString(date.getTime());
 			// AssetType with default values
 			AssetType requestAssetType = new AssetType(timestamp);
 			// Setting AssetType abbreviation to contain spaces.
-			String abbr = "Effie435"+timestamp;
+			String abbr = "Abrr has a space"+timestamp;
+			//Note: this was replacing the abbreviation at the attribute level - added the right one
 			//requestAssetType.getAttributes().get(DEFAULT_ELEMENT).setAbbreviation(abbr);
 			requestAssetType.setAbbreviation(abbr);
 			
-			int expectedRespCode = 500;
-			String expectedExceptionMsg = "com.qiotec.application.exceptions.InvalidInputException";
-			String expectedMsg = "Attribute Abbreviation Should Less Than 50 Character";
 			
 			String payload = baseHelper.toJSONString(requestAssetType);
 			ConnectionResponse conRespPost = assetTypeAPI.create(microservice, environment, payload, apiRequestHeaders);
 			ServerResponse serverResp = baseHelper.toClassObject(conRespPost.getRespBody(), ServerResponse.class);
 			
-			//Uncomment after the abbr goes back to containing spaces
-			//assertEquals("Unexpected response code", expectedRespCode, serverResp.getStatus());
-			//assertEquals("Unexpected exception message", expectedExceptionMsg, serverResp.getException());
-			//assertEquals("Unexpected error message", expectedMsg, serverResp.getMessage());
+			//Call another class for custom validations; pass the response + request from the previous class call
+			//It will grab the asset type abbr from the prev request and query assets types based on abbr - response
+			//should be nothing in this specific test 
+			//compare response of query with nothing
+			//also compare the stuff you have below
+			
+			int expectedRespCode = 500;
+			String expectedExceptionMsg = "com.qiotec.application.exceptions.InvalidInputException";
+			String expectedMsg = "Asset Type Abbreviation must not contain Spaces";
+			
+			//Ignore comment -- Uncomment after the abbr goes back to containing spaces
+			assertEquals("Unexpected response code", expectedRespCode, serverResp.getStatus());
+			assertEquals("Unexpected exception message", expectedExceptionMsg, serverResp.getException());
+			assertEquals("Unexpected error message", expectedMsg, serverResp.getMessage());
 		}
 	/*
 	 * NEGATIVE TESTS END
