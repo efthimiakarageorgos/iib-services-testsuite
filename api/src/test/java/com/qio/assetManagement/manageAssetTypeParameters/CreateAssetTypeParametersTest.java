96a8d489fc59f03c0ac0f271e719c4e87cce267d
Effie Karageorgos <effie.karageorgos@qiotec.com>
2016-10-19 22:10:39 -0400

    changed asset type parameters and asset type tests to use the reusable jars

--- api/src/test/java/com/qio/assetManagement/manageAssetTypeParameters/CreateAssetTypeParametersTest.java
+++ api/src/test/java/com/qio/assetManagement/manageAssetTypeParameters/CreateAssetTypeParametersTest.java
@@ -9,14 +9,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.qio.common.BaseTestSetupAndTearDown;
-import com.qio.lib.apiHelpers.assetType.MAssetTypeAPIHelper;
-import com.qio.lib.assertions.CustomAssertions;
-import com.qio.lib.exception.ServerResponse;
-import com.qio.model.assetType.AssetType;
-import com.qio.model.assetType.AssetTypeParameter;
-import com.qio.model.assetType.helper.AssetTypeHelper;
-import com.qio.model.assetType.helper.ParameterDataType;
-import com.qio.util.common.APITestUtil;
+import io.qio.qa.lib.ehm.apiHelpers.assetType.MAssetTypeAPIHelper;
+import io.qio.qa.lib.ehm.model.assetType.AssetType;
+import io.qio.qa.lib.ehm.model.assetType.AssetTypeParameter;
+import io.qio.qa.lib.ehm.model.assetType.helper.AssetTypeHelper;
+import io.qio.qa.lib.ehm.model.assetType.helper.ParameterDataType;
+import io.qio.qa.lib.ehm.common.APITestUtil;
+import io.qio.qa.lib.assertions.CustomAssertions;
+import io.qio.qa.lib.exception.ServerResponse;
 
 public class CreateAssetTypeParametersTest extends BaseTestSetupAndTearDown {
 
@@ -42,7 +42,7 @@ public class CreateAssetTypeParametersTest extends BaseTestSetupAndTearDown {
 		requestAssetType = assetTypeHelper.getAssetTypeWithOneParameter(ParameterDataType.String);
 		responseAssetType = APITestUtil.getResponseObjForCreate(requestAssetType, microservice, environment, apiRequestHelper, assetTypeAPI, AssetType.class);
 		assetTypeId = APITestUtil.getElementId(responseAssetType.get_links().getSelfLink().getHref());
-		assetTypeParameterId = responseAssetType.getParameters().get(FIRST_ELEMENT).getParameterId();
+		assetTypeParameterId = APITestUtil.getElementId(responseAssetType.getParameters().get(FIRST_ELEMENT).get_links().getSelfLink().getHref());
 		idsForAllCreatedElements.add(assetTypeId);
 		serverResp = new ServerResponse();
 	}
